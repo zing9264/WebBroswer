897b094069d37fb185f4606e75b04bbc44156428
crocodile <jeffchang00000@gmail.com>
2020-04-10 17:27:43 +0800

    Update Search function

--- SearchWeb/web/static/js/index.js
+++ SearchWeb/web/static/js/index.js
@@ -1,6 +1,7 @@
 
 console.log("test")
 
+/*
 data = {
   "query" : { "match" : { "content" : "測試" }}
 }
@@ -21,3 +22,75 @@ function _onResponse(response) {
 	console.log(response)
 	return response.json();
 }
+*/
+
+searchBtn = document.querySelector('input[name="searchBtn"]');
+searchBtn.addEventListener("click", searchFn);
+
+function searchFn(){
+
+	searchText = document.querySelector('input[name="search"]');
+	console.log(searchText.value)
+
+	fetchData(searchText.value)
+}
+
+
+
+function fetchData(searchText){
+
+	data = {
+	  "query" : { "match" : { "title" : "" }}
+	}
+
+	data['query']['match']['title'] = searchText
+	//console.log(data)
+
+	fetch("http://172.16.217.132:9200/test/instance/_search", {
+			contentType: 'application/json',
+			method: 'POST',
+			body: JSON.stringify(data)
+			})
+	        .then(_onResponse)
+	        .then(_onJsonReady);
+}
+
+function _onResponse(response) {
+	console.log(response)
+	return response.json();
+}
+
+function _onJsonReady(json) {
+    console.log(json);
+    console.log(json['hits']['hits'])
+    result = json['hits']['hits']
+
+    var element = document.querySelector('#resultHtml');
+    for (var i=0; i<result.length; i++){
+    	//console.log(result[i]['_source']['title'])
+    	//console.log(result[i]['_source']['content'])
+    	//console.log(result[i]['_source']['url'])
+
+    	var para = document.createElement("div");
+    	var br = document.createElement("br");
+
+    	var link = document.createElement("a");
+    	var node = document.createTextNode(result[i]['_source']['url']);
+    	link.appendChild(node);
+    	link.href = result[i]['_source']['url'];
+    	para.appendChild(link);
+    	para.appendChild(br);
+
+    	
+    	node = document.createTextNode(result[i]['_source']['title']);
+		para.appendChild(node);
+		br = document.createElement("br");
+		para.appendChild(br);
+
+		node = document.createTextNode(result[i]['_source']['content']);
+		para.appendChild(node);
+
+		element.appendChild(para);
+    }
+}
+
