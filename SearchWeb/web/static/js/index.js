3cc64a43e95f41affbd96b82f73a273ca2df1c9a
crocodile <jeffchang00000@gmail.com>
2020-04-12 16:32:03 +0800

    Update HTML

--- SearchWeb/web/static/js/index.js
+++ SearchWeb/web/static/js/index.js
@@ -1,34 +1,11 @@
 
 console.log("test")
 
-/*
-data = {
-  "query" : { "match" : { "content" : "測試" }}
-}
-
-fetch("http://172.16.217.132:9200/test/instance/_search", {
-		contentType: 'application/json',
-		method: 'POST',
-		body: JSON.stringify(data)
-		})
-        .then(_onResponse)
-        .then(_onJsonReady);
-
-function _onJsonReady(json) {
-    console.log(json);
-}
-
-function _onResponse(response) {
-	console.log(response)
-	return response.json();
-}
-*/
 
 searchBtn = document.querySelector('input[name="searchBtn"]');
 searchBtn.addEventListener("click", searchFn);
 
 function searchFn(){
-
 	searchText = document.querySelector('input[name="search"]');
 	console.log(searchText.value)
 
@@ -36,11 +13,27 @@ function searchFn(){
 }
 
 
-
 function fetchData(searchText){
 
-	data = {
-	  "query" : { "query_string" : { "query" : "",  "fields" : ["title", "content"]}}
+	data ={
+	    "query": {
+	        "query_string" : {
+	          "query" : "測試",
+	          "fields" : ["title", "content"],
+	          "analyzer": "ik_max_word"
+	        }
+	    },
+	    "size": 1000,
+	    "from": 0,
+	    "highlight": {
+	      "pre_tags": ["<span>", "<tag2>"],
+	      "post_tags": ["</span>", "</tag2>"],
+	      "fields": {
+	        "title": {},
+	        "content": {},
+	        "URL": {}
+	      }
+	    }
 	}
 
 	data['query']['query_string']['query'] = searchText
@@ -53,6 +46,7 @@ function fetchData(searchText){
 			})
 	        .then(_onResponse)
 	        .then(_onJsonReady);
+
 }
 
 function _onResponse(response) {
@@ -72,27 +66,40 @@ function _onJsonReady(json) {
     for (var i=0; i<result.length; i++){
     	//console.log(result[i]['_source']['title'])
     	//console.log(result[i]['_source']['content'])
-    	//console.log(result[i]['_source']['url'])
+    	//console.log(result[i]['_source']['URL'])
 
     	var para = document.createElement("div");
     	para.classList.add("resultHtmlList");
     	var br = document.createElement("br");
 
     	var link = document.createElement("a");
-    	var node = document.createTextNode(result[i]['_source']['url']);
+    	var node = document.createTextNode(result[i]['_source']['URL']);
     	link.appendChild(node);
-    	link.href = result[i]['_source']['url'];
+    	link.href = result[i]['_source']['URL'];
     	para.appendChild(link);
     	para.appendChild(br);
 
-    	
+    	var p = document.createElement("p");
+    	link = document.createElement("a");
+    	link.href = result[i]['_source']['URL'];
     	node = document.createTextNode(result[i]['_source']['title']);
-		para.appendChild(node);
-		br = document.createElement("br");
-		para.appendChild(br);
+    	link.classList.add("titleStyle");
+    	link.appendChild(node);
+    	p.appendChild(link);
+		para.appendChild(p);
 
-		node = document.createTextNode(result[i]['_source']['content']);
-		para.appendChild(node);
+		//console.log(result[i]['highlight']['content'][0])
+		p = document.createElement("p");
+		if(typeof(result[i]['highlight']['content']) == 'undefined'){
+			p.innerHTML = (result[i]['_source']['content']);
+
+		}
+		else{
+			p.innerHTML = result[i]['highlight']['content'][0];
+
+			
+		}
+		para.appendChild(p);
 
 		element.appendChild(para);
     }
